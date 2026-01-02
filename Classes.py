9f19dac3247489d04d359bb0d4a341662571f1a3
Francesco <franci192002@gmail.com>
2026-01-02 17:45:57 +0100

    Riposizionamento funzioni e classi

---
+++ Classes.py
@@ -0,0 +1,189 @@
+import numpy as np
+from sklearn.neighbors import NearestNeighbors
+from scipy.special import digamma
+
+
+class ActivationRecorder:
+    def __init__(self):
+        self.activations = {} # Current activations
+        self.history = {}     # History: history[epoch][layer_name] -> array
+
+    def hook(self, name):
+        def _hook(module, inputs, output): # PyTorch hook, module is a layer in the NN
+            self.activations[name] = output.detach().cpu().numpy()
+        return _hook
+
+    def InitialRegister(self, model):
+        self.activations = {}
+        self.history = {}
+
+        # Register hooks for Input
+        model.InputSpace.register_forward_hook(self.hook("input_space"))
+        
+        # Register hooks for Encoder
+        for i, layer in enumerate(model.Encoder):
+            layer.register_forward_hook(self.hook(f"encoder_layer_{i+1}"))
+
+        # Register hooks for Decoder
+        for i, layer in enumerate(model.Decoder):
+            layer.register_forward_hook(self.hook(f"decoder_layer_{i+1}"))
+
+        # Register Latent and Output
+        model.LatentSpace.register_forward_hook(self.hook("latent_space"))
+        model.OutputSpace.register_forward_hook(self.hook("output_space"))
+
+    def get(self, name):
+        return self.activations[name]
+
+    def save_epoch(self, epoch):
+        self.history[epoch] = {k: v.copy() for k, v in self.activations.items()}
+
+
+
+
+class MI_History:
+    def __init__(self):
+        self.encoder = []
+        self.decoder = []
+        self.input_latent = []
+        self.latent_output = []
+    
+    def append(self, mi_dict):
+        self.encoder.append(mi_dict["encoder"])
+        self.decoder.append(mi_dict["decoder"])
+        self.input_latent.append(mi_dict["input_latent"])
+        self.latent_output.append(mi_dict["latent_output"])
+
+    def show(self, what="all"):
+        what = what.lower().strip()
+        
+        if not self.input_latent:
+            print("No history available.")
+            return
+
+        print(f"{'='*20} MI HISTORY ({what}) {'='*20}")
+        
+        for epoch in range(len(self.input_latent)):
+            print(f"EPOCH {epoch + 1}")
+            
+            # --- Global Metrics ---
+            if what == "global" or what == "all":
+                mi_xz = self.input_latent[epoch]
+                mi_zy = self.latent_output[epoch]
+                print(f"  [Global] I(Input, Z): {mi_xz:.3f} | I(Z, Output): {mi_zy:.3f}")
+
+            # --- Encoder Layers ---
+            if what == "encoder" or what == "all":
+                print("  [Encoder]")
+                for i, (mi_in, mi_lat) in enumerate(self.encoder[epoch]):
+                    print(f"    Layer {i+1}: I(Input, L)={mi_in:.3f} | I(L, Z)={mi_lat:.3f}")
+            
+            # --- Decoder Layers ---
+            if what == "decoder" or what == "all":
+                print("  [Decoder]")
+                for i, (mi_lat, mi_out) in enumerate(self.decoder[epoch]):
+                    print(f"    Layer {i+1}: I(Z, L)={mi_lat:.3f}     | I(L, Output)={mi_out:.3f}")
+            
+            print("-" * 50)
+
+
+
+class MI_Estimator:
+    def __init__(self, method, sigma=1.0, n_neig=3):
+        self.method = method
+        self.sigma  = sigma
+        self.n_neig = n_neig
+    
+    # ---------------- KDE METHOD ----------------
+    def density(self, data):
+        N, d = data.shape
+        
+        data_sq = np.sum(data**2, axis=1, keepdims=True)
+        dists_sq = data_sq + data_sq.T - 2 * data @ data.T
+        
+        sigma_scaled = self.sigma  # self.sigma * np.sqrt(d) (Scale sigma by dimension) ***********************IMP**********************
+        
+        kernel = np.exp(-dists_sq / (2 * sigma_scaled**2))
+        return np.mean(kernel, axis=1)
+
+    def entropy_kde(self, data):
+        rho = self.density(data)
+        return -np.mean(np.log(rho + 1e-10))
+
+    # ---------------- KRASKOV METHOD ---------------- # MAI TESTATO DA VEDERE!!!!!
+    def kraskov_estimation(self, X, Y):
+        # Add tiny noise to break ties (crucial for KSG)
+        X = X + 1e-10 * np.random.rand(*X.shape)
+        Y = Y + 1e-10 * np.random.rand(*Y.shape)
+        
+        N = X.shape[0]
+        XY = np.hstack([X, Y])
+        
+        # 1. Find k-nearest neighbors in Joint Space (max norm)
+        knn = NearestNeighbors(n_neighbors=self.n_neig + 1, metric='chebyshev')
+        knn.fit(XY)
+        dists, _ = knn.kneighbors(XY)
+        
+        # Distance to the k-th neighbor
+        radii = dists[:, -1]
+        
+        # 2. Count neighbors in marginal spaces within those radii
+        # We need efficient search, so we fit new trees
+        knn_x = NearestNeighbors(metric='chebyshev').fit(X)
+        knn_y = NearestNeighbors(metric='chebyshev').fit(Y)
+        
+        # radius_neighbors returns array of arrays of indices
+        nx_indices = knn_x.radius_neighbors(X, radius=radii, return_distance=False)
+        ny_indices = knn_y.radius_neighbors(Y, radius=radii, return_distance=False)
+        
+        # Count lengths (subtract 1 because query point is included)
+        nx = np.array([len(i) - 1 for i in nx_indices])
+        ny = np.array([len(i) - 1 for i in ny_indices])
+        
+        # 3. KSG Formula
+        # MI = psi(k) + psi(N) - <psi(nx+1) + psi(ny+1)>
+        mi = (digamma(self.n_neig) + digamma(N) - 
+              np.mean(digamma(nx + 1) + digamma(ny + 1)))
+              
+        return max(0, mi)
+
+    #The idea is to compute:
+    #I(X;Z) = H(Z) - H(Z|X)
+    #Mutual information only on the stochastic layer (latent layer)
+    def LatentMutualInformation(self, mean, logVar):
+        
+        N, d = mean.shape
+        eps = 1e-10
+        var = np.exp(logVar) + eps
+        avrVar = np.mean(var)
+
+        #Conditional entropy H(Z|X)
+        H_ZgivenX = 0.5 * np.mean(np.sum(np.log(2 * np.pi * np.e * var), axis=1))
+
+        #Marginal entropy H(Z)
+        squareMean = np.sum(mean**2, axis=1, keepdims=True)
+        dists = squareMean + squareMean.T - 2 * mean @ mean.T
+
+        #Exactly as before, but with logs
+        logKernels = -dists / (2 * avrVar)
+        log_pdf_sum = np.log(np.sum(np.exp(logKernels), axis=1, keepdims=True))
+        h_Z = -np.mean(-np.log(N) + log_pdf_sum)
+
+        return h_Z - H_ZgivenX
+        
+    
+    def mutual_information(self, X, Y):
+        X = np.asarray(X)
+        Y = np.asarray(Y)  
+        # Reshape 1D arrays
+        if X.ndim == 1: X = X.reshape(-1, 1)
+        if Y.ndim == 1: Y = Y.reshape(-1, 1)
+
+        if self.method == "kde":
+            HX = self.entropy_kde(X)
+            HY = self.entropy_kde(Y)
+            HXY = self.entropy_kde(np.concatenate([X, Y], axis=1))
+            return HX + HY - HXY
+
+        if self.method == "kraskov":
+            return self.kraskov_estimation(X, Y)
---
+++ Functions.py
@@ -0,0 +1,343 @@
+import os
+import random
+from typing import List
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from matplotlib import cm
+from matplotlib.colors import Normalize
+from matplotlib.lines import Line2D
+from matplotlib.animation import FuncAnimation
+from IPython.display import HTML
+from scipy.special import digamma
+from sklearn.neighbors import NearestNeighbors
+from IPython.display import clear_output
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+import torchvision
+from torchvision import transforms
+
+
+
+def VAE_info(model, dataset, device, epoch, num_samples, mi_estimator, RecorderActivat):
+    model.eval()
+    model.to(device)
+    
+    # batch of data to evaluate Mutual Info
+    loader = torch.utils.data.DataLoader(dataset, batch_size=num_samples, shuffle=True)
+    inputs, _ = next(iter(loader))
+    inputs = inputs.to(device)
+
+    with torch.no_grad():
+        model(inputs) # Foward pass to get the activation value in RecorderActivat.activations
+
+    RecorderActivat.save_epoch(epoch)
+
+    X = inputs.view(inputs.size(0), -1).cpu().numpy()
+    Z = RecorderActivat.get("latent_space")
+    Y = RecorderActivat.get("output_space")
+        
+    mi = {
+        "encoder": [],
+        "decoder": [],
+        "input_latent": None,
+        "latent_output": None
+    }
+    
+    # Encoder Layers
+    for i in range(len(model.Encoder)):
+        layer_name = f"encoder_layer_{i+1}"
+        A = RecorderActivat.get(layer_name)
+        mi["encoder"].append((
+            mi_estimator.mutual_information(A, X), # I(Layer, Input)
+            mi_estimator.mutual_information(A, Z)  # I(Layer, Latent)
+        ))
+
+    # Decoder Layers
+    for i in range(len(model.Decoder)):
+        layer_name = f"decoder_layer_{i+1}"
+        A = RecorderActivat.get(layer_name)
+        mi["decoder"].append((
+            mi_estimator.mutual_information(A, Z), # I(Layer, Latent)
+            mi_estimator.mutual_information(A, Y)  # I(Layer, Output)
+        ))
+
+    mi["input_latent"]  = mi_estimator.mutual_information(X, Z)
+    mi["latent_output"] = mi_estimator.mutual_information(Z, Y)
+    
+    return mi
+
+
+def LatentVAE_Info(model, dataset, device, epoch, num_samples, mi_estimator, RecorderActivat):
+    model.eval()
+    model.to(device)
+
+    #Fix the same seed 
+    g = torch.Generator()
+    g.manual_seed(42)
+    
+    # batch of data to evaluate Mutual Info
+    loader = torch.utils.data.DataLoader(dataset, batch_size=num_samples, shuffle=True, generator=g)
+    inputs, _ = next(iter(loader))
+    inputs = inputs.to(device)
+
+    with torch.no_grad():
+        outputs, z, mean, logVar = model(inputs)
+
+    mean = mean.detach().cpu().numpy()
+    logVar = logVar.detach().cpu().numpy()
+
+    mi = mi_estimator.LatentMutualInformation(mean, logVar)
+
+    return mi
+
+
+
+def plot_kde_geometry(recorder, mi_estimator, part="encoder", layer=1, neuron=None, epoch=-1, bins=60):
+
+    if not recorder.history:
+        print("No history recorded yet.")
+        return
+
+    available_epochs = list(recorder.history.keys())
+    if epoch == -1:
+        epoch = available_epochs[-1]
+    if epoch not in recorder.history:
+        print(f"Epoch {epoch} not found in history.")
+        return
+
+    if part == "encoder":
+        key = f"encoder_layer_{layer}"
+    elif part == "decoder":
+        key = f"decoder_layer_{layer}"
+    elif part == "latent":
+        key = "latent_space"
+    elif part == "output":
+        key = "output_space"
+    else:
+        print("Part must be 'encoder', 'decoder', 'latent', or 'output'")
+        return
+
+    data_dict = recorder.history[epoch]
+    if key not in data_dict:
+        print(f"Key {key} not found in epoch {epoch}")
+        return
+        
+    X = data_dict[key]
+
+    if neuron is not None:
+        X = X[:, neuron:neuron+1]
+    
+    rho = mi_estimator.density(X)
+
+    X_sq = np.sum(X**2, axis=1, keepdims=True)
+    dists_sq = X_sq + X_sq.T - 2 * X @ X.T
+    dists = np.sqrt(dists_sq) # dists = np.sqrt(np.maximum(dists_sq, 0))
+    tri_idx = np.triu_indices_from(dists, k=1)
+    D = dists[tri_idx]
+
+    # for sup-title
+    title_str = f"{part}:".upper()
+    if part in ["encoder", "decoder"]:
+        title_str += f" L{layer}"
+    if neuron is not None:
+        title_str += f"-N{neuron}"        
+    title_str += f" Ep{epoch}"
+
+    fig, axs = plt.subplots(1, 2, figsize=(12, 5))
+    fig.suptitle(title_str, fontsize=14)
+
+    axs[0].hist(D, bins=bins, density=True, alpha=0.7, color='gray', edgecolor='black')
+    axs[0].set_title("Pairwise Distances")
+
+    axs[1].hist(rho, bins=bins, density=True, alpha=0.7, color='skyblue', edgecolor='black')
+    axs[1].set_title("Estimated Density (KDE)")
+    
+    plt.tight_layout()
+    plt.show()
+
+
+
+
+def PlotInfoPlane(mi_history_encoder, mi_history_decoder, title_suffix="", start_epoch=1, end_epoch=-1, Step=5):
+
+   fig = plt.figure(figsize=(16, 6))
+   gs = fig.add_gridspec(1, 3, width_ratios=[6, 6, 0.2], wspace=0.3)
+
+   ax_enc = fig.add_subplot(gs[0, 0])
+   ax_dec = fig.add_subplot(gs[0, 1])
+   ax_cb  = fig.add_subplot(gs[0, 2])
+
+   total_epochs = len(mi_history_encoder)
+   if end_epoch == -1 or end_epoch >= total_epochs:
+      end_epoch = total_epochs -1
+
+   # NEW: filter epochs by Step
+   epoch_range = [ep for ep in range(start_epoch, end_epoch + 1) if ep % Step == 0]
+   epochs = len(epoch_range)
+
+   cmap = plt.get_cmap('viridis')
+   colors = [cmap(i / max(1, epochs - 1)) for i in range(epochs)]
+
+   encoder_markers = ['o', 's', '^', 'D', 'v', 'P', '*', 'X', '<', '>']
+   decoder_markers = ['>', '*', 'p', 'X', 'H', '>', 'd', 'D', '8', 'o']
+
+   n_layers = len(mi_history_encoder[0])
+
+   # ---------------- ENCODER ----------------
+   ax = ax_enc
+
+   for ep_idx, ep in enumerate(epoch_range):
+      x = [mi_history_encoder[ep][l][0] for l in range(n_layers)]
+      y = [mi_history_encoder[ep][l][1] for l in range(n_layers)]
+
+      ax.plot(x, y, linestyle='-', color=colors[ep_idx],
+               alpha=0.6, linewidth=2)
+
+      for l in range(n_layers):
+         marker = encoder_markers[l % len(encoder_markers)]
+         ax.scatter(x[l], y[l],
+                     facecolors=[colors[ep_idx]],
+                     edgecolors='black',
+                     s=60, marker=marker,
+                     linewidths=0.6, zorder=3)
+
+   ax.set_xlabel("I(Layer; Input)", fontsize=14)
+   ax.set_ylabel("I(Layer; Latent Z)", fontsize=14)
+   ax.set_title(f"Encoder Information Plane {title_suffix}", fontsize=16)
+   ax.grid(True, alpha=0.3)
+   ax.tick_params(axis='both', labelsize=12)
+
+   enc_handles = []
+   enc_labels = []
+   for l in range(n_layers):
+      mk = encoder_markers[l % len(encoder_markers)]
+      handle = Line2D([0], [0], marker=mk, color='black',
+                     markerfacecolor='black',
+                     markeredgecolor='black',
+                     markersize=10, linestyle='None')
+      enc_handles.append(handle)
+      enc_labels.append(f'Layer {l+1}')
+
+   ax_enc.legend(enc_handles, enc_labels, framealpha=0.9, fontsize=12, title_fontsize=13)
+
+   # ---------------- DECODER ----------------
+   ax = ax_dec
+
+   for ep_idx, ep in enumerate(epoch_range):
+      x = [mi_history_decoder[ep][l][0] for l in range(n_layers)]
+      y = [mi_history_decoder[ep][l][1] for l in range(n_layers)]
+
+      ax.plot(x, y, linestyle='-', color=colors[ep_idx],
+               alpha=0.6, linewidth=2)
+
+      for l in range(n_layers):
+         marker = decoder_markers[l % len(decoder_markers)]
+         ax.scatter(x[l], y[l],
+                     facecolors=[colors[ep_idx]],
+                     edgecolors='black',
+                     s=60, marker=marker,
+                     linewidths=0.6, zorder=3)
+
+   ax.set_xlabel("I(Layer; Latent Z)", fontsize=14)
+   ax.set_ylabel("I(Layer; Output)", fontsize=14)
+   ax.set_title(f"Decoder Information Plane {title_suffix}", fontsize=16)
+   ax.grid(True, alpha=0.3)
+   ax.tick_params(axis='both', labelsize=12)
+
+   dec_handles = []
+   dec_labels = []
+   for l in range(n_layers):
+      mk = decoder_markers[l % len(decoder_markers)]
+      handle = Line2D([0], [0], marker=mk, color='black',
+                     markerfacecolor='black',
+                     markeredgecolor='black',
+                     markersize=10, linestyle='None')
+      dec_handles.append(handle)
+      dec_labels.append(f'Layer {l+1}')
+
+   ax_dec.legend(dec_handles, dec_labels, framealpha=0.9, fontsize=12, title_fontsize=13)
+
+   # ---------------- COLORBAR ----------------
+   norm = Normalize(vmin=start_epoch, vmax=end_epoch)
+   sm = cm.ScalarMappable(cmap=cmap, norm=norm)
+   sm.set_array([])
+
+   cbar = fig.colorbar(sm, cax=ax_cb, orientation='vertical')
+   cbar.set_ticks(epoch_range)
+   cbar.set_ticklabels(epoch_range)
+   cbar.set_label("Epoch", fontsize=14)
+
+   plt.show()
+
+
+def ShowSomeImages(model, testDataset, device):
+
+   model.eval()
+   fig, axs = plt.subplots(5, 2, figsize=(6, 12))
+
+   for i in range(5):
+      img, _ = random.choice(testDataset)
+
+      x = img.unsqueeze(0).to(device)
+
+      with torch.no_grad():
+         recon, _, _, _ = model(x)
+
+      original = img.cpu().squeeze().numpy()
+      reconstructed = recon.cpu().squeeze().numpy().reshape(28, 28)
+
+      axs[i, 0].imshow(original, cmap="gist_gray")
+      axs[i, 0].set_title("Original")
+      axs[i, 0].set_xticks([])
+      axs[i, 0].set_yticks([])
+
+      axs[i, 1].imshow(reconstructed, cmap="gist_gray")
+      axs[i, 1].set_title("Reconstruction")
+      axs[i, 1].set_xticks([])
+      axs[i, 1].set_yticks([])
+
+   plt.tight_layout()
+   plt.show()
+
+
+def AnimateActivations(epochsActivations, layer):
+    
+   fig, ax = plt.subplots(figsize=(8, 5))
+   
+   max_count = 0
+   for data in epochsActivations[f'Layer_{layer}']:
+      counts, _ = np.histogram(data, bins=100, range=(-1.1, 1.1))
+      if counts.max() > max_count:
+         max_count = counts.max()
+         
+   #ax.set_xlim(-1.2, 1.2)
+   ax.set_ylim(0, max_count * 1.1) # Add 10% headroom
+   
+   # Update the function (called for every frame)
+   def update(frame):
+      ax.clear() 
+      # Get data for this epoch
+      data = epochsActivations[f'Layer_{layer}'][frame]
+      
+      # Redraw settings
+      ax.hist(data, bins=100, range=(-1.1, 1.1), color='purple', alpha=0.7, edgecolor='none')
+      
+      # Re-apply limits and labels (clearing wipes them)
+      #ax.set_xlim(-1.2, 1.2)
+      ax.set_ylim(0, max_count * 1.1)
+      ax.set_title(f"Activation Distribution of layer {layer}: Epoch {frame}", fontsize=14)
+      ax.set_xlabel("Activation Value (Tanh)", fontsize=12)
+      ax.set_ylabel("Count", fontsize=12)
+      ax.grid(True, alpha=0.3)
+
+   # Create Animation
+   # interval=100 means 100ms per frame (10 fps)
+   anim = FuncAnimation(fig, update, frames=len(epochsActivations[f'Layer_{layer}']), interval=200)
+   
+   plt.close()
+   
+   # Return the HTML object
+   return HTML(anim.to_jshtml())
