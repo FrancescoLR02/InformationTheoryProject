Fixato bug overflow della mut info

Functions.py
@@ -71,14 +71,20 @@ def VAE_info(model, dataset, device, epoch, num_samples, mi_estimator, mi_histor
     # ---------------------- CALCULATE & STORE ACTIVATIONS ----------------------------
 
     with torch.no_grad():
-        model(inputs, label) # Foward pass to get the activation value in RecorderActivat.activations
+         x_hat, z, b, mean, logVar = model(inputs, label) # Foward pass to get the activation value in RecorderActivat.activations
+
+    #print(b)
+    #print(b.shape)
 
     RecorderActivat.save_epoch(epoch) # here we stored activation!
 
     # ------------------------ CALCULATE & STORE MUT.INFO ------------------------------
 
     X = inputs.view(inputs.size(0), -1).cpu().numpy()
-    Z = RecorderActivat.get("latent_space")
+    Z = RecorderActivat.get("latent_quant") # fixing here it was "latent_space" before introducing quantize latent
+    # ACTHUNG
+    #print(Z)
+    #print(Z.shape)
     Y = RecorderActivat.get("output_space")
         
     mi = {
@@ -106,7 +112,7 @@ def VAE_info(model, dataset, device, epoch, num_samples, mi_estimator, mi_histor
             mi_estimator.mutual_information(X, h, method_in_h), # I(Input, Layer)
             mi_estimator.mutual_information(h, Z, method_h_z)  # I(Layer, Latent)
         ))
-        print("x_h  h_z")
+        #print("x_h  h_z")
 
     # Decoder Layers
     for i in range(len(model.Decoder)):
@@ -116,11 +122,11 @@ def VAE_info(model, dataset, device, epoch, num_samples, mi_estimator, mi_histor
             mi_estimator.mutual_information(Z, h, method_z_h), # I(Latent, Layer)
             mi_estimator.mutual_information(h, Y, method_h_out)  # I(Layer, Output)
         ))
-        print("z_h  h_y")
+        #print("z_h  h_y")
 
     mi["input_latent"]  = mi_estimator.mutual_information(X, Z, method_in_z)  # I(Input, Latent)
     mi["latent_output"] = mi_estimator.mutual_information(Z, Y, method_z_out) # I(Latent, Output)
-    print("x_z  z_y")
+    #print("x_z  z_y")
     
     # Store the mi calculated
     mi_history.append(mi)

MI_Estimator.py
@@ -34,7 +34,6 @@ class MI_Estimator:
         self.n_neig = n_neig
         self.method = self.create_method_array(method, default)
         # for mi methods self.method = ["in_h", "h_z" ,"in_z", "z_h", "h_out", "z_out"]
-        print(self.method)
 
     def create_method_array(self, method, default):
         # if only a string is passed it is replicated 6 times
@@ -68,11 +67,10 @@ class MI_Estimator:
             return HX + HY - HXY
 
         if method_layer == "kraskov":
-            return self.kraskov_estimation(X, Y)
+            return self.entropy_kraskov(X, Y)
 
-        if method_layer == "prova":
-            print("prova")
-            return 1
+        if method_layer == "vae":
+            return self.entropy_vae(X, Y)
     
     # ------------------------- KDE METHOD -------------------------
 
@@ -92,7 +90,7 @@ class MI_Estimator:
         return np.mean(kernel, axis=1)
 
     # ------------------------- KRASKOV METHOD ------------------------- # MAI TESTATO DA VEDERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    def kraskov_estimation(self, X, Y):
+    def entropy_kraskov(self, X, Y):
         # Add tiny noise to break ties (crucial for KSG)
         X = X + 1e-10 * np.random.rand(*X.shape)
         Y = Y + 1e-10 * np.random.rand(*Y.shape)