1805a6af4146f9453e26b9a7c3e6e375695a575e
boscolomarco <boscolomarco.ing@libero.it>
2026-02-28 17:20:28 +0100

    Implementato interfaccia per metodi diversi

--- Functions.py
+++ Functions.py
@@ -55,6 +55,7 @@ from tqdm import tqdm
 #*****************************************************************************************************************
 
 # Inside here mutual informations are calculated & mut.info and also activations are stored!
+
 def VAE_info(model, dataset, device, epoch, num_samples, mi_estimator, mi_history, RecorderActivat):
 
     # -------------------------------- SETTING --------------------------------------
@@ -86,27 +87,37 @@ def VAE_info(model, dataset, device, epoch, num_samples, mi_estimator, mi_histor
         "input_latent": None,
         "latent_output": None
     }
+
+    # mi_method for each pair of layer
+    method_in_h  = mi_estimator.method[0]
+    method_h_z   = mi_estimator.method[1]
+    method_in_z  = mi_estimator.method[2]
+    method_z_h   = mi_estimator.method[3]
+    method_h_out = mi_estimator.method[4]
+    method_z_out = mi_estimator.method[5]
+
+    # recall mi_estimator.method = ["in_h", "h_z" ,"in_z", "z_h", "h_out", "z_out"]
     
     # Encoder Layers
     for i in range(len(model.Encoder)):
         layer_name = f"encoder_layer_{i+1}"
-        A = RecorderActivat.get(layer_name)
+        h = RecorderActivat.get(layer_name)
         mi["encoder"].append((
-            mi_estimator.mutual_information(A, X), # I(Layer, Input)
-            mi_estimator.mutual_information(A, Z)  # I(Layer, Latent)
+            mi_estimator.mutual_information(X, h, method_in_h), # I(Input, Layer)
+            mi_estimator.mutual_information(h, Z, method_h_z)  # I(Layer, Latent)
         ))
 
     # Decoder Layers
     for i in range(len(model.Decoder)):
         layer_name = f"decoder_layer_{i+1}"
-        A = RecorderActivat.get(layer_name)
+        h = RecorderActivat.get(layer_name)
         mi["decoder"].append((
-            mi_estimator.mutual_information(A, Z), # I(Layer, Latent)
-            mi_estimator.mutual_information(A, Y)  # I(Layer, Output)
+            mi_estimator.mutual_information(Z, h, method_z_h), # I(Latent, Layer)
+            mi_estimator.mutual_information(h, Y, method_h_out)  # I(Layer, Output)
         ))
 
-    mi["input_latent"]  = mi_estimator.mutual_information(X, Z)
-    mi["latent_output"] = mi_estimator.mutual_information(Z, Y)
+    mi["input_latent"]  = mi_estimator.mutual_information(X, Z, method_in_z)  # I(Input, Latent)
+    mi["latent_output"] = mi_estimator.mutual_information(Z, Y, method_z_out) # I(Latent, Output)
     
     # Store the mi calculated
     mi_history.append(mi)
--- MI_Estimator.py
+++ MI_Estimator.py
@@ -27,26 +27,46 @@ from IPython.display import HTML
 #*****************************************************************************************************************
 
 class MI_Estimator:
+    
 
-    def __init__(self, method, sigma=1.0, n_neig=3):
-        self.method = method
+    def __init__(self, method, sigma=1.0, n_neig=3, default="kde"):
         self.sigma  = sigma
         self.n_neig = n_neig
+        self.method = self.create_method_array(method, default)
+        # for mi methods self.method = ["in_h", "h_z" ,"in_z", "z_h", "h_out", "z_out"]
 
-    def mutual_information(self, X, Y):
+    def create_method_array(self, method, default):
+        # if only a string is passed it is replicated 6 times
+        if isinstance(method, str):
+            method_array = [method] * 6
+            return method_array
+        
+        # if a list of string is passed we fill it to 6 methods with the default type
+        if isinstance(method, (list, tuple)):
+            method_array = list(method) # convert tuple eventually into list
+
+            while len(method_array) < 6:
+                method_array.append(default)
+            return method_array[:6] # always a list of 6 strings is returned
+
+
+
+    # ------------------------- MUTUAL INFO -------------------------
+
+    def mutual_information(self, X, Y, method_layer):
         X = np.asarray(X)
         Y = np.asarray(Y)  
         # Reshape 1D arrays
         if X.ndim == 1: X = X.reshape(-1, 1)
         if Y.ndim == 1: Y = Y.reshape(-1, 1)
 
-        if self.method == "kde":
+        if method_layer == "kde":
             HX = self.entropy_kde(X)
             HY = self.entropy_kde(Y)
             HXY = self.entropy_kde(np.concatenate([X, Y], axis=1))
             return HX + HY - HXY
 
-        if self.method == "kraskov":
+        if method_layer == "kraskov":
             return self.kraskov_estimation(X, Y)
     
     # ------------------------- KDE METHOD -------------------------
