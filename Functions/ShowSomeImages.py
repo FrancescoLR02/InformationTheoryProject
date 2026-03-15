cc26af1015e9371ff4337503e31b92ae90dbc85b
boscolomarco <boscolomarco.ing@libero.it>
2026-03-15 11:51:42 +0100

    Ricostruzione resticted +-1

--- Functions/ShowSomeImages.py
+++ Functions/ShowSomeImages.py
@@ -3,8 +3,6 @@ import torch
 import matplotlib.pyplot as plt
 
 
-
-
 def ShowSomeImages(model, testDataset, device, howmany=5):
 
    model.eval()
@@ -33,3 +31,43 @@ def ShowSomeImages(model, testDataset, device, howmany=5):
 
    plt.tight_layout()
    plt.show()
+
+# ------------ resticted (round exactly -1 1) ------------------
+# --------------------------------------------------------------
+
+def ShowSomeImagesRestricted(model, testDataset, device, howmany=5):
+
+   model.eval()
+   fig, axs = plt.subplots(howmany, 2, figsize=(4, howmany*2))
+
+   for i in range(howmany):
+    img, label = random.choice(testDataset)
+
+    x = img.unsqueeze(0).to(device)
+
+    with torch.no_grad():
+         z, b, mean, logVar = model.Encoding(x, label)
+         b_round = torch.sign(b)
+         recon = model.Decoding(b_round)
+
+    original = img.cpu().squeeze().numpy()
+    reconstructed = recon.cpu().squeeze().numpy().reshape(28, 28)
+
+    axs[i, 0].imshow(original, cmap="gist_gray")
+    axs[i, 0].set_title("Original")
+    axs[i, 0].set_xticks([])
+    axs[i, 0].set_yticks([])
+
+    axs[i, 1].imshow(reconstructed, cmap="gist_gray")
+    axs[i, 1].set_title("Reconstruction")
+    axs[i, 1].set_xticks([])
+    axs[i, 1].set_yticks([])
+
+   plt.tight_layout()
+   plt.show()
+
+
+
+
+
+
